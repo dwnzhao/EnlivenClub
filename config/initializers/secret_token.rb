8966b07d7e037cb3d15286a61d4b1b9fdee5340c
Dawn Zhao <dawnzhao@outlook.com>
2017-01-11 20:47:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnlivenClub::Application.config.secret_token = 'b608d1e079040c805f632d640d65ffa2ffcd3aee07ce4ef5334f31af5ab2342bece0482ab0e74ade909b5043c54dc71d74481f92b79749bb356d364119b8d7da'
